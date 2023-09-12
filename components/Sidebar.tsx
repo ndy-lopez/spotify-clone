2badac94f50ccea1c334480893c52d0e5633ef33
Andy Loredo <ndy.loredo@gmail.com>
2023-09-12 10:42:35 -0400

    Add sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Sidebar
